e89055bffa830fc046402e606a745826e833c32b
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-11-16 13:57:49 +0000

    added loading

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import UserProfile from "./components/Profile"
-
+import { useAuth0 } from "@auth0/auth0-react";
 const App = () => {
+    const { isLoading, error } = useAuth0();
+
     return (
         <div>
             <h1>Auth0 Login</h1>
-            <LoginButton />
-            <LogoutButton />
-            <UserProfile />
+            {error && <p>There is something wrong with auth</p>}
+            {!error && isLoading && <p>Getting your details...</p>}
+            {!error && !isLoading && (
+                <>
+                    <LoginButton />
+                    <LogoutButton />
+                    <UserProfile />
+                </>
+            )}
         </div>
     )
 };
